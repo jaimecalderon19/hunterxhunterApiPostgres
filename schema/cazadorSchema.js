df0f2554a59560945eb7caa14b5c5f50ac52f75a
jaimecalderon19 <jaimebcyt@gmail.com>
2025-11-01 11:36:33 -0500

    update scheme

--- schema/cazadorSchema.js
+++ schema/cazadorSchema.js
@@ -2,16 +2,14 @@ import { pgTable, serial, text, integer, real, varchar, timestamp } from "drizzl
 
 export const cazadores = pgTable("cazadores", {
   id: serial("id").primaryKey(),
-  nombre: varchar("nombre").notNull(),
+  nombre: varchar("nombre", { length: 100 }).notNull(),
   edad: integer("edad"),
   altura: real("altura"),
   peso: real("peso"),
   imagen: text("imagen"),
-  genero: varchar("genero"),
-  habilidades: text("habilidades").array(),
-  tipoLicencia: varchar("tipo_licencia")
-    .default("Novato")
-    .notNull(),
+  genero: varchar("genero", { length: 20 }),
+  habilidades: text().array("habilidades"), // ✅ forma correcta
+  tipoLicencia: varchar("tipo_licencia", { length: 20 }).default("Novato").notNull(),
   createdAt: timestamp("created_at").defaultNow(),
   updatedAt: timestamp("updated_at").defaultNow(),
 });
